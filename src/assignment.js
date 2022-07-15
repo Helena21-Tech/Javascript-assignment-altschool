e664c992e4462c7c7f3ed05af8b62c79ec30c182
Temitope Ogunyemi <Ogunyemitemitope21@gmail.com>
2022-07-15 16:58:32 +0000

    Check challenge 1 and challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+ 
+function sumOfNumbers(arrayOfNumbers) { 
+  let sum = 0;
+  for (let number of arrayOfNumbers) {
+   sum += number;
+  }
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let value = [];
+  for (let number of arrayOfNumbers) {
+    if (number % 2 == 0) {
+      value.push(number);
+    }
+  }
+   return value.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -46,9 +58,9 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
+/*function celsiusToFahrenheit(arrayOfNumbers) {
     return arrayOfNumbers;
-}
+}*/
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
